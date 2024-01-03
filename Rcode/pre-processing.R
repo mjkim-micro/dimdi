a5c47f9f94e81012cd51c84444a51f254fed65a0
MinJi-Kim <78595847+mjkim-micro@users.noreply.github.com>
2024-01-03 17:07:25 +0900

    Update pre-processing.R

--- Rcode/pre-processing.R
+++ Rcode/pre-processing.R
@@ -11,6 +11,6 @@ phy.rel <- phyloseq::transform_sample_counts(phy, function(x) 100*x/sum(x))
 ## select four microbial biomarkers
 markers <- as.data.frame(t(otu_table(microbiome::aggregate_rare(phy.filt.rel, level = "Genus", prevalence = 50/100, detection = 0))))[,c("Alistipes", "Muribaculaceae", "Turicibacter", "Bacteroides")]
 markers.df <- as.data.frame(t(markers))
-markers.df$group <- c(rep(HC,2), rep(UH,2))
+markers.df$group <- c(rep("HC",2), rep("UH",2))
 
 View(markers.df)
